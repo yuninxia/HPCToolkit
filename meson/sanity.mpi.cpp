5900f64f1f0d410a262d6d91baa406046c687207
Jonathon Anderson <anderson.jonathonm@gmail.com>
2024-11-12 09:16:52 -0600

    Extend MPI sanity check to include link
    If the compiler environment is mangled to expose `mpi.h`, the header
    will be available even if the MPI dependency is non-functional. Use a
    full `.links()` check to identify if the MPI dependency is functional.
    
    Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>

---
+++ meson/sanity.mpi.cpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2024 Contributors to the HPCToolkit Project
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <mpi.h>
+
+int main() { // NOLINT(modernize-use-trailing-return-type)
+  MPI_Init(nullptr, nullptr);
+  int x = 42;
+  MPI_Bcast(&x, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  MPI_Finalize();
+  return 0;
+}
