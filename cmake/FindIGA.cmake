b8b86e7594c322bf42cba4df4e9fb8afd09a3b04
Jonathon Anderson <anderson.jonathonm@gmail.com>
2025-05-20 20:04:42 +0000

    Add fallback for broken IGA 2.x packaging
    Intel's packaging for the Intel Graphics Compiler (IGC) and Assembler
    (IGA) is missing the unversioned symlink (lib*.so). This is very
    unconventional, but work around the issue by looking for the current
    versioned file (lib*.so.2) as a fallback.
    
    The code tries to ensure the library it finds matches the headers, it
    only searches the prefix where the headers are installed (assuming they
    are installed under `{prefix}/include` like normal). It also searches
    the "use this one dammit" environment variable IGA_ROOT.
    
    Note that the version is hardcoded here, so if IGC 3.x releases
    without this being fixed this code will likely do more harm than good.
    Caveat emptor.
    
    Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>

--- cmake/FindIGA.cmake
+++ cmake/FindIGA.cmake
@@ -7,6 +7,21 @@ find_path(IGA_INCLUDE_DIR
 find_library(IGA_LIBRARY
   NAMES iga64)
 
+# As of IGC 2.1.12, the libiga64.so symlink is missing. This was reported but we're
+# still awaiting a fix: https://github.com/intel/intel-graphics-compiler/issues/360
+# Work around this by searching for the library by its SONAME (libigc64.so.2) where we
+# found the header (in addition to IGA_ROOT). This is sort of an evil/fragile approach
+# that probably will be _very_ problematic once IGC 3.x releases.
+if(IGA_INCLUDE_DIR AND NOT IGA_LIBRARY)
+  cmake_path(GET IGA_INCLUDE_DIR PARENT_PATH IGA_INCLUDE_ROOT)
+  find_library(IGA_LIBRARY_2
+    HINTS "${IGA_INCLUDE_ROOT}/lib/${CMAKE_LIBRARY_ARCHITECTURE}" "${IGA_INCLUDE_ROOT}/lib"
+    NAMES iga64 libiga64.so.2
+    NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH NO_SYSTEM_ENVIRONMENT_PATH
+    NO_CMAKE_SYSTEM_PATH)
+  set(IGA_LIBRARY "${IGA_LIBRARY_2}")
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(IGA
   FOUND_VAR IGA_FOUND
