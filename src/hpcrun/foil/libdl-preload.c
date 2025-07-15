30d9318e5b2602ff6a5eb21ad569091c131a5dc0
John Mellor-Crummey <johnmc@rice.edu>
2025-07-15 16:23:35 -0500

    fix formatting issues
    Signed-off-by: John Mellor-Crummey <johnmc@rice.edu>

--- src/hpcrun/foil/libdl-preload.c
+++ src/hpcrun/foil/libdl-preload.c
@@ -9,9 +9,6 @@
 //***************************************************************************
 
 #define _GNU_SOURCE
-
-
-
 #include <dlfcn.h>
 #include <elf.h>
 #include <pthread.h>
@@ -24,10 +21,13 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+//******************************************************************************
+// local includes
+//******************************************************************************
+
 #include "../audit/audit-api.h"
 #include "../hpcrun-sonames.h"
 #include "common.h"
-
 
 static bool verbose = false;
 static const char* vdso_path = NULL;
@@ -215,7 +215,7 @@ const auditor_exports_t* hpcrun_connect_to_auditor() {
   export_symbols(&exports);
 
   verbose = getenv("HPCRUN_AUDIT_DEBUG"); // invokes libc getenv
-  vdso_path = "[vdso]"; // Set later to something more reasonable.
+  vdso_path = "[vdso]";                   // Set later to something more reasonable.
 
   return &exports;
 }
