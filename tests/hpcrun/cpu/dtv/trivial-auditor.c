4e0339964f293b5f181737bb02ce96025220611a
John Mellor-Crummey <johnmc@rice.edu>
2025-10-09 12:53:32 -0700

    tests: add dtv test to check auditor dtv patch
    Signed-off-by: John Mellor-Crummey <johnmc@rice.edu>

---
+++ tests/hpcrun/cpu/dtv/trivial-auditor.c
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Contributors to the HPCToolkit Project
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#define _GNU_SOURCE
+#include <link.h>
+
+unsigned int la_version(unsigned int version) { return LAV_CURRENT; }
