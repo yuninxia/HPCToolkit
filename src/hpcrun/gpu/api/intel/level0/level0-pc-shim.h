0cdd0e1253dd5686476fcd7b822c147eba3b3496
Yuning Xia <yx87@rice.edu>
2025-10-22 00:07:01 +0000

    hpcrun: change the license from BSD-3 to Apache-2.0 for intel pcsampling
    Signed-off-by: Yuning Xia <yx87@rice.edu>

--- src/hpcrun/gpu/api/intel/level0/level0-pc-shim.h
+++ src/hpcrun/gpu/api/intel/level0/level0-pc-shim.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: Contributors to the HPCToolkit Project
 //
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: Apache-2.0
 
 #ifndef _LEVEL0_PC_SHIM_H_
 #define _LEVEL0_PC_SHIM_H_
--- src/hpcrun/gpu/api/intel/level0/pcsampling/level0-correlation-device-map.h
+++ src/hpcrun/gpu/api/intel/level0/pcsampling/level0-correlation-device-map.h
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: Contributors to the HPCToolkit Project
 //
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: Apache-2.0
 
 #ifndef LEVEL0_CORRELATION_DEVICE_MAP_H
 #define LEVEL0_CORRELATION_DEVICE_MAP_H
