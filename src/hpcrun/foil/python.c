353939ee68012873b157a4cb70ee06402bfba735
John Mellor-Crummey <johnmc@rice.edu>
2025-10-22 19:47:56 -0500

    tests: adjust python foil a different way
    rather than returning false immediately if dlopen returns 0, just make
    sure that we never dlclose(NULL)
    
    Signed-off-by: John Mellor-Crummey <johnmc@rice.edu>

--- src/hpcrun/foil/python.c
+++ src/hpcrun/foil/python.c
@@ -40,11 +40,10 @@ bool f_identify_libpython(const char* path) {
     return false;
   }
   void* libpy = dlopen(path, RTLD_LAZY | RTLD_NOLOAD);
-  if (libpy == NULL) {
-    return false;
-  }
   if (dlsym(libpy, "PySys_AddAuditHook") == NULL) {
-    dlclose(libpy);
+    if (libpy != NULL) {
+      dlclose(libpy);
+    }
     return false;
   }
   dispatch = (struct Dispatch){
