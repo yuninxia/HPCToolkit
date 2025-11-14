54824326f989be07595090c7e5c23195f21f526a
John Mellor-Crummey <johnmc@rice.edu>
2025-11-14 00:10:51 -0800

    hpcrun: cuda pc sampling: don't care about stalls unless not issued
    Signed-off-by: John Mellor-Crummey <johnmc@rice.edu>

--- src/hpcrun/gpu/api/nvidia/cupti-activity-translate.c
+++ src/hpcrun/gpu/api/nvidia/cupti-activity-translate.c
@@ -242,7 +242,9 @@ convert_pcsampling
   set_gpu_instruction_fields(&ga->details.instruction, activity->correlationId,
            activity->functionId, activity->pcOffset, correlation_id);
 
-  ga->details.pc_sampling.issue_stall_reason = convert_stall_type(activity->stallReason);
+  // stall reason only matters if latency is exposed (activity->latencySamples > 0)
+  ga->details.pc_sampling.issue_stall_reason = activity->latencySamples > 0 ?
+    convert_stall_type(activity->issue_stall_reason) : GPU_INST_STALL_DONTCARE;
   ga->details.pc_sampling.issues = activity->samples;
   ga->details.pc_sampling.non_issues = activity->latencySamples;
   ga->details.pc_sampling.issue_stall_exposed = activity->latencySamples > 0;
